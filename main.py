8fbbdd73e513dfcd84fb2ffd6d9a68861c99aa64
NitricXd <Nitricxd@gmail.com>
2022-06-20 13:10:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -307,157 +307,157 @@ START_TIME = datetime.now()
 
 if bot1:
     bot1.start()
-    bot1.join_chat("TheXCodeTeam")
+    bot1.join_chat("XCodeSupport")
 if bot2:
     bot2.start()
-    bot2.join_chat("TheXCodeTeam")
+    bot2.join_chat("XCodeSupport")
 if bot3:
     bot3.start()
-    bot3.join_chat("TheXCodeTeam")
+    bot3.join_chat("XCodeSupport")
 if bot4:
     bot4.start()
-    bot4.join_chat("TheXCodeTeam")
+    bot4.join_chat("XCodeSupport")
 if bot5:
     bot5.start()
-    bot5.join_chat("TheXCodeTeam")
+    bot5.join_chat("XCodeSupport")
 if bot6:
     bot6.start()
-    bot6.join_chat("TheXCodeTeam")
+    bot6.join_chat("XCodeSupport")
 if bot7:
     bot7.start()
-    bot7.join_chat("TheXCodeTeam")
+    bot7.join_chat("XCodeSupport")
 if bot8:
     bot8.start()
-    bot8.join_chat("TheXCodeTeam")
+    bot8.join_chat("XCodeSupport")
 if bot9:
     bot9.start()
-    bot9.join_chat("TheXCodeTeam")
+    bot9.join_chat("XCodeSupport")
 if bot:
     bot.start()
-    bot.join_chat("TheXCodeTeam")
+    bot.join_chat("XCodeSupport")
 if bot11:
     bot11.start()
-    bot11.join_chat("TheXCodeTeam")
+    bot11.join_chat("XCodeSupport")
 if bot12:
     bot12.start()
-    bot12.join_chat("TheXCodeTeam")
+    bot12.join_chat("XCodeSupport")
 if bot13:
     bot13.start()
-    bot12.join_chat("TheXCodeTeam")
+    bot12.join_chat("XCodeSupport")
 if bot14:
     bot14.start()
-    bot14.join_chat("TheXCodeTeam")
+    bot14.join_chat("XCodeSupport")
 if bot15:
     bot15.start()
-    bot15.join_chat("TheXCodeTeam")
+    bot15.join_chat("XCodeSupport")
 if bot16:
     bot16.start()
-    bot16.join_chat("TheXCodeTeam")
+    bot16.join_chat("XCodeSupport")
 if bot17:
     bot17.start()
-    bot17.join_chat("TheXCodeTeam")
+    bot17.join_chat("XCodeSupport")
 if bot18:
     bot18.start()
-    bot18.join_chat("TheXCodeTeam")
+    bot18.join_chat("XCodeSupport")
 if bot19:
     bot19.start()
-    bot19.join_chat("TheXCodeTeam")
+    bot19.join_chat("XCodeSupport")
 if bot20:
     bot20.start()
-    bot20.join_chat("TheXCodeTeam")
+    bot20.join_chat("XCodeSupport")
 if bot21:
     bot21.start()
-    bot21.join_chat("TheXCodeTeam")
+    bot21.join_chat("XCodeSupport")
 if bot22:
     bot22.start()
-    bot22.join_chat("TheXCodeTeam")
+    bot22.join_chat("XCodeSupport")
 if bot23:
     bot23.start()
-    bot23.join_chat("TheXCodeTeam")
+    bot23.join_chat("XCodeSupport")
 if bot24:
     bot24.start()
-    bot24.join_chat("TheXCodeTeam")
+    bot24.join_chat("XCodeSupport")
 if bot25:
     bot25.start()
-    bot25.join_chat("TheXCodeTeam")
+    bot25.join_chat("XCodeSupport")
 if bot26:
     bot26.start()
-    bot26.join_chat("TheXCodeTeam")
+    bot26.join_chat("XCodeSupport")
 if bot27:
     bot27.start()
-    bot27.join_chat("TheXCodeTeam")
+    bot27.join_chat("XCodeSupport")
 if bot28:
     bot28.start()
-    bot28.join_chat("TheXCodeTeam")
+    bot28.join_chat("XCodeSupport")
 if bot29:
     bot29.start()
-    bot29.join_chat("TheXCodeTeam")
+    bot29.join_chat("XCodeSupport")
 if bot30:
     bot30.start()
-    bot30.join_chat("TheXCodeTeam")
+    bot30.join_chat("XCodeSupport")
 if bot31:
     bot31.start()
-    bot31.join_chat("TheXCodeTeam")
+    bot31.join_chat("XCodeSupport")
 if bot32:
     bot32.start()
-    bot32.join_chat("TheXCodeTeam")
+    bot32.join_chat("XCodeSupport")
 if bot33:
     bot33.start()
-    bot33.join_chat("TheXCodeTeam")
+    bot33.join_chat("XCodeSupport")
 if bot34:
     bot34.start()
-    bot34.join_chat("TheXCodeTeam")
+    bot34.join_chat("XCodeSupport")
 if bot35:
     bot35.start()
-    bot35.join_chat("TheXCodeTeam")
+    bot35.join_chat("XCodeSupport")
 if bot36:
     bot36.start()
-    bot36.join_chat("TheXCodeTeam")
+    bot36.join_chat("XCodeSupport")
 if bot37:
     bot37.start()
-    bot37.join_chat("TheXCodeTeam")
+    bot37.join_chat("XCodeSupport")
 if bot38:
     bot38.start()
-    bot38.join_chat("TheXCodeTeam")
+    bot38.join_chat("XCodeSupport")
 if bot39:
     bot39.start()
-    bot39.join_chat("TheXCodeTeam")
+    bot39.join_chat("XCodeSupport")
 if bot40:
     bot40.start()
-    bot40.join_chat("TheXCodeTeam")
+    bot40.join_chat("XCodeSupport")
 if bot41:
     bot41.start()
-    bot41.join_chat("TheXCodeTeam")
+    bot41.join_chat("XCodeSupport")
 if bot42:
     bot42.start()
-    bot42.join_chat("TheXCodeTeam")
+    bot42.join_chat("XCodeSupport")
 if bot43:
     bot43.start()
-    bot43.join_chat("TheXCodeTeam")
+    bot43.join_chat("XCodeSupport")
 if bot44:
     bot44.start()
-    bot44.join_chat("TheXCodeTeam")
+    bot44.join_chat("XCodeSupport")
 if bot45:
     bot45.start()
-    bot45.join_chat("TheXCodeTeam")
+    bot45.join_chat("XCodeSupport")
 if bot46:
     bot46.start()
-    bot46.join_chat("TheXCodeTeam")
+    bot46.join_chat("XCodeSupport")
 if bot47:
     bot47.start()
-    bot47.join_chat("TheXCodeTeam")
+    bot47.join_chat("XCodeSupport")
 if bot48:
     bot48.start()
-    bot48.join_chat("TheXCodeTeam")
+    bot48.join_chat("XCodeSupport")
 if bot49:
     bot49.start()
-    bot49.join_chat("TheXCodeTeam")
+    bot49.join_chat("XCodeSupport")
 if bot50:
     bot50.start()
-    bot50.join_chat("TheXCodeTeam")
+    bot50.join_chat("XCodeSupport")
 
 idle()
 
 print("🎉 Successfully Deployed 🎉 @Xd_Nitric")
-print("Enjoy! Do visit @TheXCodeTeam")
+print("Enjoy! Do visit @XCodeSupport")
 
