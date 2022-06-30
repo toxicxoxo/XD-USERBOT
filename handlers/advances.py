a911f37c0fd7a79ad44eaa334b40f4b482dc8a73
NitricXd <Nitricxd@gmail.com>
2022-06-30 15:04:21 +0530

    Update advances.py

--- handlers/advances.py
+++ handlers/advances.py
@@ -276,7 +276,7 @@ async def pornspam(xspam: Client, e: Message):
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["ping"], [".", "!", "/"]))
 async def oahgfg(xspam: Client, e: Message):
-      await e.reply_text(f"═══════════════════\n ꧁ 𒈞zαι∂ υѕєявσт𒈞꧂ \n═══════════════════")
+      await e.reply_text(f"═══════════════════\n ꧁ 𒈞Xᴅ Uѕєявσт𒈞꧂ \n═══════════════════")
 
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["broadcast", "gcast"], [".", "!", "/"]))
